495641ec5e349074790c3e3731256696369eaa5e
Tarun Kushwah <tarunkushwahkt@gmail.com>
2026-02-22 18:35:02 +0530

    Print neigbhour node

---
+++ print.py
@@ -0,0 +1,15 @@
+def create_graph(edges, n):
+    graph = {i: [] for i in range(n)}
+    
+    for u, v in edges:
+        graph[u].append(v)
+        graph[v].append(u)
+    
+    return graph
+
+edges = [(0, 1), (0, 2), (1, 3), (2, 4)]
+n = 5
+
+graph = create_graph(edges, n)
+for node in graph:
+    print(node, "->", graph[node])
