52f2d814e9fcbd76160ab21a9d2d63741f106ccf
Tarun Kushwah <tarunkushwahkt@gmail.com>
2026-02-28 16:32:17 +0530

    unreachable_pairs

--- unreachable_pairs.py
+++ unreachable_pairs.py
@@ -3,10 +3,8 @@ def count_unreachable_pairs(n, edges):
     for u, v in edges:
         graph[u].append(v)
         graph[v].append(u)
-
     visited = [False] * n
     component_sizes = []
-
     def dfs(node):
         stack = [node]
         size = 0
@@ -19,11 +17,9 @@ def count_unreachable_pairs(n, edges):
                     visited[nei] = True
                     stack.append(nei)
         return size
-
     for i in range(n):
         if not visited[i]:
             component_sizes.append(dfs(i))
-
     total_pairs = 0
     remaining = n
     for size in component_sizes:
