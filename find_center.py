e858d374280e9365906055bec9a8ecc3ff1691cb
Tarun Kushwah <tarunkushwahkt@gmail.com>
2026-03-03 19:28:58 +0530

    Finding center by graph'

--- find_center.py
+++ find_center.py
@@ -3,7 +3,6 @@ def find_center(edges):
     c, d = edges[1]
     return a if a == c or a == d else b
 if __name__ == "__main__":
-    # Example usage
     edges = [[1, 2], [2, 3], [4, 2]]
     center = find_center(edges)
     print("Center of the star graph:", center)
