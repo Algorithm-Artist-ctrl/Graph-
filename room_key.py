f3cc77f727e49dd9ba237fed6467f5f80088268d
Tarun Kushwah <tarunkushwahkt@gmail.com>
2026-03-05 18:14:44 +0530

    Room and key

---
+++ room_key.py
@@ -0,0 +1,18 @@
+def can_visit_all_rooms(rooms):
+    visited = set()
+
+    def dfs(room):
+        if room in visited:
+            return
+        visited.add(room)
+        for key in rooms[room]:
+            dfs(key)
+
+    dfs(0)
+    return len(visited) == len(rooms)
+rooms = [[1], [2], [3], []]
+
+if can_visit_all_rooms(rooms):
+    print("All rooms can be visited")
+else:
+    print("All rooms cannot be visited")
