b84ee67bc48629162621c248395cdb987438b2d4
Tarun Kushwah <tarunkushwahkt@gmail.com>
2026-03-06 20:42:44 +0530

    check for room and key

--- room_key.py
+++ room_key.py
@@ -7,11 +7,9 @@ def can_visit_all_rooms(rooms):
         visited.add(room)
         for key in rooms[room]:
             dfs(key)
-
     dfs(0)
     return len(visited) == len(rooms)
 rooms = [[1], [2], [3], []]
-
 if can_visit_all_rooms(rooms):
     print("All rooms can be visited")
 else:
